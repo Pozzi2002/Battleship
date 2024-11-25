edf8708c8bf597a660b2794151d4629f569bb561
Pozzi2002 <nik.stroganov.2019@mail.ru>
2024-11-25 18:42:02 +0300

    commit before windows repair

--- src/scripts/primaryPage/gameBoardBindPlayer.js
+++ src/scripts/primaryPage/gameBoardBindPlayer.js
@@ -1,11 +1,11 @@
 import { player1, Ship } from "./../logic/SubjectsGame.js"
 function bindPlayer() {
-let firstPlayerGrid = document.querySelector(".firstPlayerGrid2").children;
 firstPlayerGrid.forEach(renderContent)
 
-
 player1.gameboard.forEach(arr => arr.forEach(item => {
-   
+    let i = 0;
+    let j = 0
+    document.getElementById(`${i}-${j}`).textContent = item
  })
 );
 
