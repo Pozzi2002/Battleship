f7f28a73e8f4eb4a44d4b9b780666fa2b7f4bfe4
Pozzi2002 <nik.stroganov.2019@mail.ru>
2024-11-26 14:08:55 +0300

    bind gameboadrd DOM with object

--- src/scripts/primaryPage/gameBoardBindPlayer.js
+++ src/scripts/primaryPage/gameBoardBindPlayer.js
@@ -1,15 +1,15 @@
 import { player1, Ship } from "./../logic/SubjectsGame.js"
 function bindPlayer() {
-firstPlayerGrid.forEach(renderContent)
+const firstPlayerGrid = document.querySelector('.firstPlayerGrid2').children;
+let total = []
 
-player1.gameboard.forEach(arr => arr.forEach(item => {
-    let i = 0;
-    let j = 0
-    document.getElementById(`${i}-${j}`).textContent = item
- })
-);
-
-}
+player1.gameboard.forEach(arr => arr.forEach(item => total.push(item)));
+  
+Array.from(firstPlayerGrid).forEach(grid => {
+  grid.textContent = total[0];
+  total.shift()
+ });
+};
 
 
 
