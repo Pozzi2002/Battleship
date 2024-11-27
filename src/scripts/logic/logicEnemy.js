e323111b3dcbc401fece38b18a6b72e73095da72
Pozzi2002 <nik.stroganov.2019@mail.ru>
2024-11-27 20:38:19 +0300

    light tunned logic enemy, but hi's super dumb

--- src/scripts/logic/logicEnemy.js
+++ src/scripts/logic/logicEnemy.js
@@ -6,65 +6,42 @@ let yData;
 let hit = false;
 
 function logicEnemyAttack(n = 0) {
-  let x = randomNumber();
-  let y = randomNumber();
-
+  let [x, y] = randomNumber();
   if (n > 2000) return;
-  console.log(x, y)
   if (player1.gameboard[x][y] === 0) {
-    player1.receiveAttack(y, x);
-    bindPlayer();
-    hit = false;
+      player1.receiveAttack(y, x);
+      bindPlayer();
+      hit = false;
   } else if (player1.gameboard[x][y] === 1) {
-    player1.receiveAttack(y, x);
-    bindPlayer();
-    xData = x;
-    yData = y;
-    hit = true;
+      player1.receiveAttack(y, x);
+      bindPlayer();
+      xData = x;
+      yData = y;
+      hit = true;
   } else if (
-    player1.gameboard[x][y] === 2 ||
-    player1.gameboard[x][y] === 'X'
+      player1.gameboard[x][y] === 2 ||
+      player1.gameboard[x][y] === 'X'
   ) {
-    hit = false;
-    logicEnemyAttack(n + 1);
+      hit = false;
+      logicEnemyAttack(n + 1);
   }
 };
 
 function randomNumber() {
   if (hit === true) {
     if (Math.random() > 0.5) {
-      return xData += 1;
+      return [ xData += 1, yData ];
     } else {
-      return yData += 1;
+      return [ xData, yData += 1 ];
     }
   } else {
-  return Math.round(Math.random() * 8);
+    let randomX = Math.round(Math.random() * 8); 
+    let randomY = Math.round(Math.random() * 8);
+  return [ randomX, randomY ]
  }
 };
+
 /*Logic enemy
-xData;
-yData;
-hit;
 
-if player1 gameboard == 1
-xData = x
-yData = y
-hit = true;
-
-if player1 gameboard == 'X' || 2 || 0 
-hit = false
-
-randomNumber
-if (hit === true) {
-  if (Math.random() > 0.5) {
-  return xData += 1
-  } else {
-   return yData += 1
-  }
-} else {
- xData = 0;
- yData = 0;
-return random
-}
 */
 export { logicEnemyAttack }
